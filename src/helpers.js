6d6ddbb04521481bf1d5f2c5ead0897310bdbaff
Walther Aguirre <benjy_2976@hotmail.com>
2023-04-12 09:19:23 -0500

    enable to check whit an array

--- src/helpers.js
+++ src/helpers.js
@@ -1,78 +1,85 @@
 export function normalizeRelations(data, relations) {
-    return {
-        ...data,
-        ...relations.reduce(
-            (prev, relation) => ({
-                ...prev,
-                [relation.attribute]: Array.isArray(data[relation.attribute]) ?
-                    data[relation.attribute].map(x => x.id) : data[relation.attribute].id
-            }), {}
-        )
-    };
+  return {
+    ...data,
+    ...relations.reduce(
+      (prev, relation) => ({
+        ...prev,
+        [relation.attribute] : Array.isArray(data[relation.attribute]) ?
+          data[relation.attribute].map(x => x.id) : data[relation.attribute].id
+      }), {}
+    )
+  };
 }
 
 export function resolveRelations(data, state, rootGetters, level=1) {
+  if(state.relations.length==0){
+    return data
+  }else{
     return {
-        ...data,
-        ...state.relations.reduce(
-            (prev, relation) => {
-                let alias = relation.alias != undefined ? relation.alias : relation.attribute
+      ...data,
+      ...state.relations.reduce(
+        (prev, relation) => {
+          let alias = relation.alias != undefined ? relation.alias : relation.attribute
 
-                if(state.maxRelationsResolve>=level){
-                    return ({
-                        ...prev,
-                        [alias]: relationLinck(data, alias, relation, state.key, rootGetters, level+1)
-                    })
-                }
-                else{
-                    return ({
-                        ...prev,
-                        ['errorResolve']: 'Max relation resolve exceded: resolved '+level+' times'
-                    })
-                }
+          if(state.maxRelationsResolve>=level){
+            return ({
+              ...prev,
+              [alias] : relationLinck(data, alias, relation, state.key, rootGetters, level+1)
+            })
+          }
+          else{
+            return ({
+              ...prev,
+              ['errorResolve'] : 'Max relation resolve exceded: resolved '+level+' times'
+            })
+          }
 
-            }, {}
-        )
+        }, {}
+      )
     };
+  }
 }
 
 function relationLinck(data, alias, relation, key, rootGetters, level) {
-    if (relation.hasMany === false) {
-        return Array.isArray(data[relation.alias]) ?
-            data[relation.alias].map(x => rootGetters[`${relation.module}/find`](x, level)) :
-            rootGetters[`${relation.module}/find`](data[relation.attribute], level)
-    } else {
-        return rootGetters[`${relation.module}/filter`](d => d[relation.attribute] === data[key], level)
-    }
+  if (relation.hasMany === false) {
+    return Array.isArray(data[relation.alias]) ?
+      data[relation.alias].map(x => rootGetters[`${relation.module}/find`](x, level)) :
+      rootGetters[`${relation.module}/find`](data[relation.attribute], level)
+  } else {
+    return rootGetters[`${relation.module}/filter`](d => d[relation.attribute] === data[key], level)
+  }
 }
 
 export function exportRelations(data, state, dispatch, rootGetters) {
-    if(data.pivot!==undefined){
-        delete data.pivot
-      }
+  if(data.pivot!==undefined){
+    delete data.pivot
+  }
+  if(state.relations.length==0){
+    return data
+  }else
     return {
-        ...data,
-        ...state.relations.reduce(
-            (prev, relation) => {
-                let attr = data[relation.alias]
-                if (attr !== undefined) {
-                    if(Array.isArray(attr)){
-                        prev[relation.alias]= attr.map(obj => obj[rootGetters[`${relation.module}/key`]])
-                    }
-                    else{
-                        delete prev[relation.attribute]
-                    }
-                    dispatch(`${relation.module}/sync`, attr, { root: true })
+      ...data,
+      ...state.relations.reduce(
+        (prev, relation) => {
+          let attr = data[relation.alias]
+          if (attr !== undefined) {
+            if(Array.isArray(attr)){
+              prev[relation.alias]= attr.map(obj => obj[rootGetters[`${relation.module}/key`]])
+            }
+            else{
+              delete prev[relation.attribute]
+            }
+            dispatch(`${relation.module}/sync`, attr, { root: true })
 
-                    return ({
-                        ...prev
-                    })
-                }
-                else {
-                    return { ...prev }
-                }
-            }, {}
-        )
+            return ({
+              ...prev
+            })
+          }
+          else {
+            return { ...prev }
+          }
+        }, {}
+      )
     };
     /* for (const index in state.relations) {
         let relation = state.relations[index]
@@ -82,7 +89,7 @@ export function exportRelations(data, state, dispatch, rootGetters) {
             data[relation.attribute] = data[relation.attribute].map(obj => obj[rootGetters[`${relation.module}/key`]])
         }
     } */
-    /* state.relations.forEach(relation => {
+  /* state.relations.forEach(relation => {
         let attr = data[relation.attribute]
         if (attr !== undefined && Array.isArray(attr) && attr.length > 0 && typeof attr[0] === 'object') {
             dispatch(`${relation.module}/syncItems`, attr, { root: true })
